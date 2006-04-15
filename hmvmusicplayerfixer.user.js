ec825cc9407fa2f5c211035d138d08007ef4b4f5
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2006-04-15 13:50:51 +0000

    Added HMV Music Player Fixer
    darcs-hash:20060415135051-39dcc-b3ba1bdbab44bb299013a274890e8fb64c461048.gz

---
+++ hmvmusicplayerfixer.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           HMV Music Player Fixer
+// @namespace      http://insin.woaf.net
+// @description    Fixes the function used to control the music player
+// @include        http://hmv.co.uk/*
+// @include        http://www.hmv.co.uk/*
+// ==/UserScript==
+(
+    function()
+    {
+        if (typeof(unsafeWindow.playClip) == "function")
+        {
+            unsafeWindow.playClip = function(url)
+            {
+                unsafeWindow.document.clipstream.stop_audio();
+                unsafeWindow.document.clipstream.set_audiostreamurl(0, url + ".32");
+                unsafeWindow.document.clipstream.play_audio();
+            }
+        }
+    }
+)();
