ef60f2fe054600e4b00112873c8c03f8c5206eea
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2007-08-08 21:43:33 +0000

    Updated Rllmuk Tidy
    darcs-hash:20070808214333-39dcc-a4cf4a5774d8cdc475649180ba69e76becfe0fb7.gz

--- rllmuktidy.user.js
+++ rllmuktidy.user.js
@@ -1,122 +1,96 @@
 // ==UserScript==
 // @name        Rllmuk Tidy
 // @namespace   http://www.jonathanbuchanan.plus.com/repos/greasemonkey/
-// @description Applies CSS fixes to neaten layout & attempts to tidy up posts
+// @description Attempts to tidy up posts.
 // @include     http://www.rllmukforum.com/*
+// @include     http://rllmukforum.com/*
+// @include     http://www.rpsoft.co.uk/*
+// @include     http://rpsoft.co.uk/*
+// @include     http://www.extranoise.co.uk/*
+// @include     http://extranoise.co.uk/*
 // ==/UserScript==
 
-(
-function()
+// Select all post text
+var textnodes = document.evaluate(
+    "//div[@class='postcolor']//text()",
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null);
+
+// If post text was found, attempt to tidy it up
+if (textnodes.snapshotLength > 0)
 {
-    // Apply CSS
-    var css =
-'.formbuttonrow div img[src*="p_up.gif"], .formbuttonrow div img[src*="p_card.gif"] { display: none !important; }\
-.catend { height: 1px !important; padding: 0 !important; }\
-.subtitle { display: none !important; }\
-#ipbwrapper + table { display: none !important; }\
-#gfooter { display: none !important; }\
-body > div:first-child { display: none !important; }\
-body > div:first-child + div { display: none !important; }\
-.post2 .postdetails br, .post1 .postdetails br { display: none !important; }\
-.post1 .postdetails img, .post2 .postdetails img { display: block !important; margin-bottom: 1em !important; }\
-.quotetop { border-left: 1px dotted black !important; }\
-.quotemain { border-left: 1px dotted black !important; padding-bottom: 0 !important; }';
+    var ciReplacements, csReplacements, nonWordReplacements, regex, key, node, s;
 
-    head = document.getElementsByTagName("head")[0];
-    style = document.createElement("style");
-    style.setAttribute("type", "text/css");
-    style.innerHTML = css;
-    head.appendChild(style);
+    regex = {};
 
-    // Select all post text
-    var textnodes = document.evaluate(
-        "//div[@class='postcolor']//text()",
-        document,
-        null,
-        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-        null);
-
-    // If post text was found, attempt to tidy it up
-    if (textnodes.snapshotLength > 0)
+    // Case insensitive replacements
+    ciReplacements =
     {
-        var ciReplacements, csReplacements, nonWordReplacements, regex, key, node, s;
+        // Mis-spellings
+        "teh":"the",
+        "sentance":"sentence",
+        "sequal":"sequel",
+        "definately":"definitely",
+        "arguements":"arguments",
+        "rediculous":"ridiculous",
+        "grammer":"grammar",
 
-        regex = {};
+        // Grammar and punctuation
+        "would of":"would have",
+        "could of":"would have",
+        "should of":"should have",
+        "must of":"must have",
+        "alot":"a lot",
+        "infact":"in fact"
+    };
+    for (key in ciReplacements)
+    {
+        regex[key] = new RegExp("\\b" + key + "\\b", "i");
+    }
 
-        // Case insensitive replacements
-        ciReplacements =
-        {
-            // Mis-spellings
-            "teh":"the",
-            "sentance":"sentence",
-            "sequal":"sequel",
-            "definately":"definitely",
-            "arguements":"arguments",
-            "rediculous":"ridiculous",
-            "grammer":"grammar",
+    // Case sensitive replacements
+    csReplacements =
+    {
+        // Grammar and punctuation
+        "im":"I'm",
+        "i'":"I'",
+        "^i\\s":"I ",
+        "\\si\\s":" I ",
+        "\\si$":" I"
+    };
+    for (key in csReplacements)
+    {
+        regex[key] = new RegExp("\\b" + key + "\\b");
+    }
 
-            // Grammar and punctuation
-            "would of":"would have",
-            "could of":"would have",
-            "should of":"should have",
-            "must of":"must have",
-            "alot":"a lot",
-            "infact":"in fact"
-        };
+    // Non-word replacements
+    nonWordReplacements =
+    {
+        "!(?:[!1]|one)+":"!" // Replaces !!11one style exclamations with a single !
+    };
+    for (key in nonWordReplacements)
+    {
+        regex[key] = new RegExp(key, "i");
+    }
+
+    for (var i = 0; i < textnodes.snapshotLength; i++)
+    {
+        node = textnodes.snapshotItem(i);
+        s = node.data;
         for (key in ciReplacements)
         {
-            regex[key] = new RegExp("\\b" + key + "\\b", "i");
+            s = s.replace(regex[key], ciReplacements[key]);
         }
-
-        // Case sensitive replacements
-        csReplacements =
-        {
-            // Grammar and punctuation
-            "im":"I'm",
-            "i'":"I'",
-            "^i\\s":"I ",
-            "\\si\\s":" I ",
-            "\\si$":" I"
-        };
         for (key in csReplacements)
         {
-            regex[key] = new RegExp("\\b" + key + "\\b");
+            s = s.replace(regex[key], csReplacements[key]);
         }
-
-        // Non-word replacements
-        nonWordReplacements =
-        {
-            "!(?:[!1]|one)+":"!" // Replaces !!11one style exclamations with a single !
-        };
         for (key in nonWordReplacements)
         {
-            regex[key] = new RegExp(key, "i");
+            s = s.replace(regex[key], nonWordReplacements[key]);
         }
-
-        for (var i = 0; i < textnodes.snapshotLength; i++)
-        {
-            node = textnodes.snapshotItem(i);
-            s = node.data;
-            for (key in ciReplacements)
-            {
-                s = s.replace(regex[key], ciReplacements[key]);
-            }
-            for (key in csReplacements)
-            {
-                s = s.replace(regex[key], csReplacements[key]);
-            }
-            for (key in nonWordReplacements)
-            {
-                s = s.replace(regex[key], nonWordReplacements[key]);
-            }
-            node.data = s;
-        }
+        node.data = s;
     }
 }
-)();
-
-/* Acknowledgements
- * ----------------
- * Post tidying section based on DumbQuotes by Mark Pilgrim
- * - http://diveintogreasemonkey.org/casestudy/dumbquotes.html
- * -------------------------------------------------------------------------- */
