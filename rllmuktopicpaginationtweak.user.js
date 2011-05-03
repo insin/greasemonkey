8a6fb5f0688b69357201c031ed7e1d0b6b556670
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-05-03 11:01:04 +0100

    Added Rllmuk Topic Pagination Tweak

---
+++ rllmuktopicpaginationtweak.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name           Rllmuk Topic Pagination Tweak
+// @namespace      https://github.com/insin/greasemonkey
+// @description    Changes topic pagination links from 1-2-3-99 format to 1-97-98-99 format
+// @include        http://rllmukforum.com/*
+// @include        http://www.rllmukforum.com/*
+// ==/UserScript==
+
+// Don't do anything if we're not on a topic listing page
+if (window.location.href.indexOf("module=search") == -1 &&
+    window.location.href.indexOf("/index.php?showforum=") == -1) {
+  return;
+}
+
+var pageNumberRE = /(\d+)/;
+var startIndexRE = /&st=(\d+)/;
+
+var paginators = document.querySelectorAll("ul.mini_pagination");
+
+for (var i = 0, paginator; paginator = paginators[i]; i++) {
+  var links = paginator.getElementsByTagName("a");
+  if (links.length < 4) {
+    // Nothing to do - less than 4 links
+    continue;
+  }
+
+  var lastPage = parseInt(links[3].textContent, 10);
+  if (lastPage == 4) {
+    // Nothing to do - pages 1-4 being displayed
+    continue;
+  }
+
+  // Determine number of posts per page from second link
+  var postsPerPage = parseInt(startIndexRE.exec(links[1].href)[1], 10);
+
+  // Set new page numbers and start indices middle 2 links
+  links[1].href = links[2].href.replace(startIndexRE, "&st=" + (postsPerPage * (lastPage - 3)));
+  links[1].textContent = lastPage - 2;
+  links[1].title = "Go to page " + (lastPage - 2);
+  links[2].href = links[2].href.replace(startIndexRE, "&st=" + (postsPerPage * (lastPage - 2)));
+  links[2].textContent = lastPage - 1;
+  links[2].title = "Go to page " + (lastPage - 1);
+}
