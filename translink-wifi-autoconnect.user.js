cf7263795f490b67431956207b9e3c38250ec5bb
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2015-01-15 09:01:25 +0000

    Added Translink WiFi Autoconnect user script

---
+++ translink-wifi-autoconnect.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Translink WiFi Autoconnect
+// @namespace   https://github.com/insin/greasemonkey/
+// @description Automatically submits the Translink WiFi connection T&C page
+// @include     https://portal.moovmanage.com/translink*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var checkbox = document.querySelector('#aup_agree')
+checkbox.checked = true
+checkbox.form.submit()
