422f9c208d417b12a11f64f957f6d73bbdc1bfaf
Jonny Buchanan <jonathan.buchanan@gmail.com>
2019-02-27 21:43:23 +1000

    [rllmukzen-threadshitter] v4
    Hide posts which @-mention ignored users

--- rllmukzen-threadshitter.user.js
+++ rllmukzen-threadshitter.user.js
@@ -2,7 +2,7 @@
 // @name        Rllmukzen Threadshitter
 // @description Really ignore ignored users
 // @namespace   https://github.com/insin/greasemonkey/
-// @version     3
+// @version     4
 // @match       https://www.rllmukforum.com/index.php*
 // ==/UserScript==
 
@@ -22,28 +22,46 @@ function TopicPage() {
     }
   `)
 
-  // Hide posts which quote ignored users
-  function processQuotes(quotes) {
-    quotes.forEach(el => {
-      if (!ignoredUserIds.includes(el.dataset.ipsquoteUserid)) return
-      let comment = el.closest('article.ipsComment')
-      if (comment.style.display == 'none') return
-      comment.style.display = 'none'
+  // Hide posts containing elements which have an ignored user id as a specified
+  // data attribute.
+  function hidePostsByDataAttribute(elements, dataAttribute) {
+    elements.forEach(el => {
+      if (!ignoredUserIds.includes(el.dataset[dataAttribute])) return
+      let post = el.closest('article.ipsComment')
+      if (post.style.display == 'none') return
+      post.style.display = 'none'
     })
   }
 
-  function processPage() {
-    processQuotes(document.querySelectorAll('[data-ipsquote-userid]'))
+  // Hide posts which quote ignored users
+  function processQuotes(context) {
+    hidePostsByDataAttribute(
+      context.querySelectorAll('[data-ipsquote-userid]'),
+      'ipsquoteUserid'
+    )
+  }
+
+  // Hide posts which @-mention ignored users
+  function processMentions(context) {
+    hidePostsByDataAttribute(
+      context.querySelectorAll('[data-mentionid]'),
+      'mentionid'
+    )
+  }
+
+  function processPosts(context = document) {
+    processQuotes(context)
+    processMentions(context)
   }
 
   // Process initial posts
-  processPage()
+  processPosts()
 
   // Watch for posts being replaced when paging
   new MutationObserver(mutations =>
     mutations.forEach(mutation => {
       if (mutation.oldValue == 'true') {
-        processPage()
+        processPosts()
       }
     })
   ).observe(document.querySelector('div.cTopic'), {
@@ -54,11 +72,9 @@ function TopicPage() {
 
   // Watch for new posts being loaded into the current page
   new MutationObserver(mutations =>
-    mutations.forEach(mutation => {
-      mutation.addedNodes.forEach(post => {
-        processQuotes(post.querySelectorAll('[data-ipsquote-userid]'))
-      })
-    })
+    mutations.forEach(mutation =>
+      mutation.addedNodes.forEach(processPosts)
+    )
   ).observe(document.querySelector('#elPostFeed > form'), {
     childList: true,
   })
@@ -67,7 +83,9 @@ function TopicPage() {
 function IgnoredUsersPage() {
   // Sync ignored user ids
   localStorage.ignoredUserIds = JSON.stringify(
-    Array.from(document.querySelectorAll('[data-ignoreuserid]')).map(el => el.dataset.ignoreuserid)
+    Array.from(document.querySelectorAll('[data-ignoreuserid]')).map(el =>
+      el.dataset.ignoreuserid
+    )
   )
 }
 
